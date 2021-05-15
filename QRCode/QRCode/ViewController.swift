886500a489d2aef8d6cf61d48c8bdb96c62a0e76
vudat81299 <vudat81299@gmail.com>
2021-05-15 08:11:54 +0700

    vudat081299

---
+++ QRCode/QRCode/ViewController.swift
@@ -0,0 +1,194 @@
+//
+//  ViewController.swift
+//  QRCode
+//
+//  Created by Vũ Quý Đạt  on 14/05/2021.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
+    }
+
+
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+
+        dismiss(animated: true)
+    }
+
+    func found(code: String) {
+        
+        print(aes256?.decrypt(data: Data(code.utf8)))
+        print(code)
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+
+}
+
+
+
+// MARK: - Encrypt.
+import Foundation
+import CommonCrypto
+
+struct AES {
+
+    // MARK: - Value
+    // MARK: Private
+    private let key: Data
+    private let iv: Data
+
+
+    // MARK: - Initialzier
+    init?(key: String, iv: String) {
+        guard key.count == kCCKeySizeAES128 || key.count == kCCKeySizeAES256, let keyData = key.data(using: .utf8) else {
+            debugPrint("Error: Failed to set a key.")
+            return nil
+        }
+
+        guard iv.count == kCCBlockSizeAES128, let ivData = iv.data(using: .utf8) else {
+            debugPrint("Error: Failed to set an initial vector.")
+            return nil
+        }
+
+
+        self.key = keyData
+        self.iv  = ivData
+    }
+
+
+    // MARK: - Function
+    // MARK: Public
+    func encrypt(string: String) -> Data? {
+        return crypt(data: string.data(using: .utf8), option: CCOperation(kCCEncrypt))
+    }
+
+    func decrypt(data: Data?) -> String? {
+        guard let decryptedData = crypt(data: data, option: CCOperation(kCCDecrypt)) else { return nil }
+        return String(bytes: decryptedData, encoding: .utf8)
+    }
+
+    func crypt(data: Data?, option: CCOperation) -> Data? {
+        guard let data = data else { return nil }
+
+        let cryptLength = data.count + kCCBlockSizeAES128
+        var cryptData   = Data(count: cryptLength)
+
+        let keyLength = key.count
+        let options   = CCOptions(kCCOptionPKCS7Padding)
+
+        var bytesLength = Int(0)
+
+        let status = cryptData.withUnsafeMutableBytes { cryptBytes in
+            data.withUnsafeBytes { dataBytes in
+                iv.withUnsafeBytes { ivBytes in
+                    key.withUnsafeBytes { keyBytes in
+                    CCCrypt(option, CCAlgorithm(kCCAlgorithmAES), options, keyBytes.baseAddress, keyLength, ivBytes.baseAddress, dataBytes.baseAddress, data.count, cryptBytes.baseAddress, cryptLength, &bytesLength)
+                    }
+                }
+            }
+        }
+
+        guard UInt32(status) == UInt32(kCCSuccess) else {
+            debugPrint("Error: Failed to crypt data. Status \(status)")
+            return nil
+        }
+
+        cryptData.removeSubrange(bytesLength..<cryptData.count)
+        return cryptData
+    }
+}
+let password = "UserPassword1!"
+let key128   = "1234567890123456"                   // 16 bytes for AES128
+let key256   = "12345678901234561234567890123456"   // 32 bytes for AES256
+let iv       = "abcdefghijklmnop"                   // 16 bytes for AES128
+
+let aes128 = AES(key: key128, iv: iv)
+let aes256 = AES(key: key256, iv: iv)
+
+//let encryptedPassword128 = aes128?.encrypt(string: password)
+//aes128?.decrypt(data: encryptedPassword128)
+
+let encryptedPassword256 = aes256?.encrypt(string: password)
